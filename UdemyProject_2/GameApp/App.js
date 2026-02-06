6fbf4ea3002da5875e8295160c0dd44ebe93343c
velobmidav <vadim.belov@finshape.com>
2026-02-06 13:06:47 +0100

    Added background image component.

--- UdemyProject_2/GameApp/App.js
+++ UdemyProject_2/GameApp/App.js
@@ -1,17 +1,22 @@
-import { StyleSheet, View} from 'react-native';
+import { StyleSheet, View, ImageBackground} from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
   <LinearGradient colors={["#ace6dfff","#eb2a97ff"]} style={styles.rootScreen} >
+    <ImageBackground source={require("./assets/background.png")} resizeMode='cover' style={styles.rootScreen} imageStyle={styles.backgroundImage}>
       <StartGameScreen/>
+    </ImageBackground>
   </LinearGradient>
   )
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
-    flex: 1
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.05
   }
 });
