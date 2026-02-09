9083822a10867771880040d953ea2ad499b4449a
velobmidav <vadim.belov@finshape.com>
2026-02-09 12:14:57 +0100

    Created simple structure for gameScreen screen.

--- UdemyProject_2/GameApp/screens/GameScreen.js
+++ UdemyProject_2/GameApp/screens/GameScreen.js
@@ -1,7 +1,21 @@
-import {Text} from "react-native"
+import {Text,View, StyleSheet} from "react-native"
 
 function GameScreen () {
-    return <Text>Game screen</Text>;
+    return <View style={styles.screen}>
+        <Text>Game screen</Text>
+        <View>
+            <Text>Higher or lower?</Text>
+        </View>
+        <View>
+        </View>
+        </View>;
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 12
+    }
+})
